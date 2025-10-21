0fd1f43a202d713b75c7499bfa6f96adff6b25e2
Moritz Knoll <mknoll@k0r4p10.42mulhouse.fr>
2025-10-21 13:19:53 +0200

    started C01

--- ex01/myAwesomePhoneBook.hpp
+++ ex01/myAwesomePhoneBook.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   myAwesomePhoneBook.hpp                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moritzknoll <moritzknoll@student.42.fr>    +#+  +:+       +#+        */
+/*   By: mknoll <mknoll@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 11:39:09 by moritzknoll       #+#    #+#             */
-/*   Updated: 2025/10/21 11:42:20 by moritzknoll      ###   ########.fr       */
+/*   Updated: 2025/10/21 12:49:41 by mknoll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,35 @@
 #include <cctype>
 #include <iomanip>
 
+class Contact {
+    private: 
+        std ::string first_name;
+        std ::string last_name;
+        std ::string nickname;
+        std ::string darkest_secret;
+        std ::string phone_number;
+    public: 
+    
+};
+
 
 class PhoneBook {
     public: 
-}
+        Contact contacts[8];
+        // contact
+        void ADD(Contact &c){
+            
+        }
+        void SEARCH(Contact &c){
+            // display a list of 4 columns indeex first name, last name, nickname
+            // each column must be 10 charactes wide 
+            // seperatet by a pipe 
+            // text must be right aligned 
+            // if longer than 10 
+        }
+        void EXIT(){
+            // quit and delete all contacts 
+        }
+};
 
 
-class Contract {
-    public: 
-        
-}
--- ex01/myAwesomePhonebook.cpp
+++ ex01/myAwesomePhonebook.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   myAwesomePhonebook.cpp                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: moritzknoll <moritzknoll@student.42.fr>    +#+  +:+       +#+        */
+/*   By: mknoll <mknoll@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/21 10:10:59 by moritzknoll       #+#    #+#             */
-/*   Updated: 2025/10/21 11:39:28 by moritzknoll      ###   ########.fr       */
+/*   Updated: 2025/10/21 12:31:17 by mknoll           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,19 @@
 
 int main(int argc, char *argv[])
 {
-
-
-
-
+    if (argv[1] == "ADD")
+    {
+        
+    }
+    else if (argv[1] == "EXIT")
+    {
+        PhoneBook.EXIT();
+    }
+    else if (argv[1] == "SEARCH")
+    {
+        
+    }
+    else
+        return (std::cout << "Not applicable", 0);
     return (0); 
 }
