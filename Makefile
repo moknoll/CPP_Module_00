ffef36621f6c64445f739b72c698a4048fa709e3
Moritz Knoll <moritz.knoll@gmx.net>
2025-10-21 12:09:47 +0200

    ex01

---
+++ Makefile
@@ -0,0 +1,7 @@
+#Compiler
+CXX = g++
+
+#Compiler Flags
+CXX_FLAGS = -Wall -Werror -Wextra -std=-c++98
+
+#
---
+++ ex01/myAwesomePhoneBook.hpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   myAwesomePhoneBook.hpp                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: moritzknoll <moritzknoll@student.42.fr>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/21 11:39:09 by moritzknoll       #+#    #+#             */
+/*   Updated: 2025/10/21 11:42:20 by moritzknoll      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+#include <cctype>
+#include <iomanip>
+
+
+class PhoneBook {
+    public: 
+}
+
+
+class Contract {
+    public: 
+        
+}
